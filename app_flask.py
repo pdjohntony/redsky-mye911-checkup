a9b904adbd71f87ba52d59fc59be0846729dfe4f
Phill Johntony <phidjohn@cdw.com>
2021-08-31 22:17:54 -0400

    Major changes Changed some column names Generates a second file in CSV format for RS importing Changed GUI

--- app_flask.py
+++ app_flask.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, escape, request, render_template, url_for, flash, redirect
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask import Flask, escape, request, render_template, url_for, flash, redirect, send_from_directory
+from flask_uploads import UploadSet, configure_uploads
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 # from werkzeug.utils import secure_filename
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField, validators
 from flask_bootstrap import Bootstrap
 
 import pandas as pd
@@ -22,11 +22,12 @@ You can use Flask-Reuploaded as a drop-in replacement to Flask-Uploads, which fi
 '''
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+upload_dest = os.path.join(basedir, 'uploads')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bacon'
-app.config['UPLOADED_CSVS_DEST'] = os.path.join(basedir, 'uploads') # you'll need to create a folder named uploads
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 # sets maximum file size to 5MB
+app.config['UPLOADED_CSVS_DEST'] = upload_dest # you'll need to create a folder named uploads
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 # sets maximum file size to 5MB
 Bootstrap(app)
 
 csv_set = UploadSet('csvs')
@@ -34,8 +35,9 @@ configure_uploads(app, csv_set)
 
 class UploadForm(FlaskForm):
 	users_csv = FileField('Users', validators=[FileRequired('File was empty!'), FileAllowed(['csv'], 'CSV only!')])
-	maps_csv = FileField('Device Mappings', validators=[FileRequired('File was empty!'), FileAllowed(['csv'], 'CSV only!')])
-	devices_csv = FileField('All Devices', validators=[FileRequired('File was empty!'), FileAllowed(['csv'], 'CSV only!')])
+	maps_csv = FileField('MyE911 Device Mappings', validators=[FileRequired('File was empty!'), FileAllowed(['csv'], 'CSV only!')])
+	devices_csv = FileField('Devices', validators=[FileRequired('File was empty!'), FileAllowed(['csv'], 'CSV only!')])
+	pbx_name = StringField('PBX Name', [validators.required()])
 	submit = SubmitField('Upload')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -47,7 +49,8 @@ def home():
 		filename1 = csv_set.save(form.users_csv.data)
 		filename2 = csv_set.save(form.maps_csv.data)
 		filename3 = csv_set.save(form.devices_csv.data)
-		csv_urls = [csv_set.url(filename1), csv_set.url(filename2), csv_set.url(filename3)]
+		csv_urls = [filename1, filename2, filename3, form.pbx_name.data]
+		# csv_urls = [csv_set.url(filename1), csv_set.url(filename2), csv_set.url(filename3)]
 
 		report_results = generate_report(csv_urls)
 		# flash('Files uploaded successfully.')
@@ -57,12 +60,19 @@ def home():
 		report_results = None
 	return render_template('home.html', form=form, file_url=csv_urls, results=report_results)
 
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config['UPLOADED_CSVS_DEST'], name)
+
 def generate_report(csv_urls):
-	fname_users   = csv_urls[0]
-	fname_maps    = csv_urls[1]
-	fname_devices = csv_urls[2]
-	fname_output_fol = "uploads\\"
-	fname_output     = "redsky-users-and-devicemappings-"+datetime.datetime.now().strftime("%Y.%m.%d-%I.%M.%S")+".xlsx"
+	fname_users   = os.path.join(upload_dest, csv_urls[0])
+	fname_maps    = os.path.join(upload_dest, csv_urls[1])
+	fname_devices = os.path.join(upload_dest, csv_urls[2])
+	pbx_name      = csv_urls[3]
+	fname_output_fol  = "uploads\\"
+	fname_output      = "mye911-checkup-"+datetime.datetime.now().strftime("%Y.%m.%d-%I.%M.%S")+".xlsx"
+	fname_bulk_output = "missing-mye911-device_mappings-"+datetime.datetime.now().strftime("%Y.%m.%d-%I.%M.%S")+".csv"
+	NoneType = type(None)
 
 	#* 1 Load users csv file
 	df_users = pd.read_csv(fname_users)
@@ -86,7 +96,7 @@ def generate_report(csv_urls):
 	print(f"File loaded successfully: {fname_maps}")
 	# Rename username column
 	df_maps.rename({'## MyE911 Username': 'Username'}, axis='columns', inplace=True)
-	df_maps.rename({'Device Identifier': 'Device Mapping'}, axis='columns', inplace=True)
+	df_maps.rename({'Device Identifier': 'Existing Device Map'}, axis='columns', inplace=True)
 	# Delete unnecessary columns
 	del df_maps["PBX Name"]
 
@@ -94,7 +104,7 @@ def generate_report(csv_urls):
 	# Left merge the dataframes, this will fill in device mappings to any user that has a mapping, almost like an excel vlookup
 	df_merge = pd.merge(df_users, df_maps, on ='Username', how ='left')
 	df_merge.sort_values(by=["Username"], ascending=True, inplace=True)
-	df_merge["Mapable"] = None
+	df_merge["Found Device Map"] = None
 
 	#* 5 Load all devices csv file
 	df_devices = pd.read_csv(fname_devices)
@@ -106,7 +116,7 @@ def generate_report(csv_urls):
 
 	#* 6 Iterate over merged dataframe, look for null in Device Mapping column, then search through devices dataframe for match
 	for index, row in df_merge.iterrows():
-		if type(row["Device Mapping"]) == float: # if column is NULL / np.nan (float)
+		if type(row["Existing Device Map"]) == float: # if column is NULL / np.nan (float)
 			d_match = df_devices.loc[df_devices["Device Name"] == "CSF"+row["Username"]]
 			# print(type(d_match))
 			# print(d_match)
@@ -119,14 +129,14 @@ def generate_report(csv_urls):
 				else:
 					# print("Match found "+str(d_match.iloc[0]["Device Name"]))
 					#! WRITE MATCH INFO INTO MERGE DF
-					df_merge.loc[index, "Mapable"] = "Yes"
+					df_merge.loc[index, "Found Device Map"] = str(d_match.iloc[0]["Device Name"])
 
 
 	print("\nGenerating report...")
 	stat_users_total          = len(df_merge.index)
-	stat_users_with_device    = len(df_merge[df_merge["Device Mapping"].notnull()])
-	stat_users_without_device = len(df_merge[df_merge["Device Mapping"].isnull()])
-	stat_devices_available    = len(df_merge[df_merge["Mapable"].notnull()])
+	stat_users_with_device    = len(df_merge[df_merge["Existing Device Map"].notnull()])
+	stat_users_without_device = len(df_merge[df_merge["Existing Device Map"].isnull()])
+	stat_devices_available    = len(df_merge[df_merge["Found Device Map"].notnull()])
 	print(f"Total MyE911 Users: {stat_users_total}")
 	print(f"Users with a device mapped: {stat_users_with_device}")
 	print(f"Users without a device mapped: {stat_users_without_device}")
@@ -149,7 +159,8 @@ def generate_report(csv_urls):
 	worksheet.set_column('D:D', 14)
 	worksheet.set_column('E:E', 12)
 	worksheet.set_column('F:F', 11, format_text)
-	worksheet.set_column('G:G', 16)
+	worksheet.set_column('G:G', 21)
+	worksheet.set_column('H:H', 21)
 	af_range = "A1:H"+str(df_merge.shape[0]+1)
 	worksheet.autofilter(af_range)
 	# worksheet.auto_filter.add_filter_column(1, ['INVALID'], blank=False)
@@ -158,7 +169,33 @@ def generate_report(csv_urls):
 	# csv_set.save(writer.save())
 	print(f"Report saved as: {fname_output}")
 
-	return [stat_users_total, stat_users_with_device, stat_users_without_device, stat_devices_available, fname_output]
+	if stat_devices_available > 0:
+		df_merge.reset_index(inplace = True, drop = True)
+		indexes_to_drop = []
+		for index, row in df_merge.iterrows():
+			# print(f"Type={type(row['Found Device Map'])}, Value={row['Found Device Map']}")
+			if type(row["Found Device Map"]) == NoneType:
+				# print("removing null")
+				indexes_to_drop.append(index)
+		df_merge.drop(df_merge.index[indexes_to_drop], inplace=True)
+
+		# Delete uneccessary columns for the bulk CSV import file
+		del df_merge["Email"]
+		del df_merge["First Name"]
+		del df_merge["Last Name"]
+		del df_merge["Role"]
+		del df_merge["ELIN"]
+		del df_merge["Existing Device Map"]
+
+		df_merge.rename({'Username': 'MyE911 Username'}, axis='columns', inplace=True)
+		df_merge.rename({'Found Device Map': 'Device Identifier'}, axis='columns', inplace=True)
+
+		df_merge["PBX Name"] = pbx_name
+
+		df_merge.to_csv(fname_output_fol+fname_bulk_output, index=False)
+		print(f"Bulk Import file saved as: {fname_bulk_output}")
+
+	return [stat_users_total, stat_users_with_device, stat_users_without_device, stat_devices_available, fname_output, fname_bulk_output]
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5001)
